661bf2dd500c470950b6c81e22e5728d77744db7
abc <abc@example.com>
2025-03-07 23:34:43 +0500

    first

--- src/app/html/page.js
+++ src/app/html/page.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import styles from "./html.module.css";
 export  default function JsPage () {
   return (
     <div>
-        <h1> HTML</h1>
+        <h1 className={styles.h1}> HTML</h1>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+
       <header className={styles.header}>
         <h1 className={styles.h1}>NazDev</h1>
 
@@ -16,11 +18,21 @@ export default function Home() {
       </header>
 
       <div className={styles.content}>
-          <h1>NazDev Стань лучшим разработчиком с нами!</h1>
+
+        <div className={styles.mainBlock}>
+           <h1>NazDev <br/>Стань лучшим  <br/><span>разработчиком</span></h1>
+           <h1 className={styles.fronth1}>Frontend</h1>
+        </div>
+
+        <footer className={styles.footer}>
+         <div className={styles.itemfooter}><p>Java script</p></div>
+         <div className={styles.itemfooter}><p>html</p></div>
+         <div className={styles.itemfooter}><p>css</p></div>
+         <div className={styles.itemfooter}><p>react</p></div>
+        </footer>
+        <p className={styles.footertitle}>NazDev corporation</p>
+
       </div>
-
-
-
     </div>
   );
 }
