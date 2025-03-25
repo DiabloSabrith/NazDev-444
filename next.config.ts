ca9501bfd46e2084e1d33ae9f1aa2c75dd24f6e2
abc <abc@example.com>
2025-03-25 22:57:25 +0500

    NazDev-tem-PWA

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+  const withPWA = require('next-pwa')({
+    dest: 'public', //  Где будут сгенерированы файлы PWA (манифест, SW)
+    register: true,   // Автоматическая регистрация service worker
+    skipWaiting: true,  //  Service worker активируется сразу
+    disable: process.env.NODE_ENV === 'development', //  Отключаем в dev-режиме
+    //  Необязательные опции (важны для production)
+    // runtimeCaching: [], //  Управление кешированием ресурсов
+    // buildExcludes: [], // Исключение файлов из сборки PWA
+})
 
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    // Другие настройки Next.js
+}
+
+module.exports = withPWA(nextConfig)
