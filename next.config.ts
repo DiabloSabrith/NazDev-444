8d8cd73ba649397bf3e1a790bd95ef7842e47e03
abc <abc@example.com>
2025-03-26 00:12:40 +0500

    555

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,22 @@
-
-  const withPWA = require('next-pwa')({
-    dest: 'public', //  Где будут сгенерированы файлы PWA (манифест, SW)
-    register: true,   // Автоматическая регистрация service worker
-    skipWaiting: true,  //  Service worker активируется сразу
-    disable: process.env.NODE_ENV === 'development', //  Отключаем в dev-режиме
-    //  Необязательные опции (важны для production)
-    // runtimeCaching: [], //  Управление кешированием ресурсов
-    // buildExcludes: [], // Исключение файлов из сборки PWA
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public', //  Где будут сгенерированы файлы PWA (манифест, SW)
+  register: true,   // Автоматическая регистрация service worker
+  skipWaiting: true,  //  Service worker активируется сразу
+  disable: process.env.NODE_ENV === 'development', //  Отключаем в dev-режиме
+  //  Необязательные опции (важны для production)
+  // runtimeCaching: [], //  Управление кешированием ресурсов
+  // buildExcludes: [], // Исключение файлов из сборки PWA
 })
 
 const nextConfig = {
-    reactStrictMode: true,
-    // Другие настройки Next.js
+  reactStrictMode: true,
+  // Другие настройки Next.js
 }
 
 module.exports = withPWA(nextConfig)
+
+
+
+
+
