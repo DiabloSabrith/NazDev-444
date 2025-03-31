5c2591a33ddbebbec21eba890436205a99821f44
abc <abc@example.com>
2025-03-31 14:07:31 +0500

    update work page

--- src/app/js/bigint/page.tsx
+++ src/app/js/bigint/page.tsx
@@ -1,81 +1,41 @@
 'use client'
 import React, {useState,useEffect} from 'react'
 import styles from "./page.module.scss";
-import Image from 'next/image'
-
-
 import Link  from "next/link"
 import 'animate.css';
 import WarningVideo from "@/components/warningVideo/warningVideo";
-import IntTable from "@/components/IntaractivTable/IntTable";
-import Accordion from "@/components/accordion/accordion";
+
+
 
 export  default function JsPage () {
 
     return(
         <div className={styles.container}>
-            <div className={styles.wrapper}>
-                    <div className={styles.content}>
+            <div className={styles.wrapper}> 
 
-            
-
-                    <p>Пример на СodePen</p>
-                    <button className={styles.codePenBtn}>Посмотреть пример </button>
-
-            
-
-                            <div className={styles.codePen}>
-
-                                <iframe className={styles.codPen}
-
-                                        height="300"
-                                        style={{ width: '80%' }} /* Используем объект стилей */
-                                        scrolling="no"
-                                        title="Basic HTML, CSS, and JavaScript"
-                                        src="https://codepen.io/Diablo-Sabrith/embed/qEBppOw?default-tab=js%2Cresult"
-                                        frameBorder="no"
-                                        loading="lazy"
-
-                                >
-                                    See the Pen{' '}
-                                    <a href="https://codepen.io/Diablo-Sabrith/pen/qEBppOw">
-                                        Basic HTML, CSS, and JavaScript
-                                    </a>{' '}
-                                    by Diablo-Sabrith (<a href="https://codepen.io/Diablo-Sabrith">@Diablo-Sabrith</a>) on{' '}
-                                    <a href="https://codepen.io">CodePen</a>.
-                                </iframe>
-                            </div>
-                    </div>
-                <div className={styles.textBlock}>
-                   <div className={styles.infoContent}>
-
-                       <div  className={styles.infoContent_One} >
-                           <p>
-                           В JavaScript стандартный тип данных Number имеет ограничение на максимальное значение, которое он может безопасно представлять. Это может стать проблемой при работе с большими числами  
-                           Именно здесь на помощь приходит тип данных BigInt. BigInt позволяет представлять целые числа произвольной длины. Он был добавлен в JavaScript (ES2020) для решения проблемы точности при работе с числами, выходящими за пределы
-                              </p>
-                       </div>
-
-
-                       <div className={styles.infoContent_Two}>
-                           <div className={styles.itemOne}>
-                               <Image className={styles.img}
-                                      src="/img/js.jpg"
-                                      width={240}
-                                      height={240}
-                                      alt="Loading..."
-                               />
-                           </div>
-                           <div className={styles.itemTwo}>
-                               <button className={styles.backBtn}>Назад</button>
-                               <button className={styles.nextBtn}>Далее</button>
-                           </div>
-                       </div>
-                   </div>
-                </div>
                 <div className={styles.videoBlock}>
                     <WarningVideo/>
                 </div>
+
+                <div className={styles.cursItemBlock}>
+
+                    <div className={styles.cursItem}>
+                        <h3>Циклы while и for</h3>
+                        <p>Не пройди мимо!</p>
+                        <button className={styles.btn}>Начать</button>
+                    </div>
+                    <div className={styles.cursItem}>
+                        <h3>Функции</h3>
+                        <p>Просто о сложном</p>
+                        <button className={styles.btn}>Начать</button>
+                    </div>
+                    <div className={styles.cursItem}>
+                        <h3>Работа с DOM</h3>
+                        <p>Почти Junior</p>
+                        <button className={styles.btn}>Начать</button>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
--- src/app/js/cycle/page.tsx
+++ src/app/js/cycle/page.tsx
@@ -1,88 +1,41 @@
-import React from 'react'
+'use client'
+import React, {useState,useEffect} from 'react'
 import styles from "./page.module.scss";
-import Image from 'next/image'
 import Link  from "next/link"
 import 'animate.css';
-import Accordion from "@/components/accordion/accordion";
+import WarningVideo from "@/components/warningVideo/warningVideo";
+
+
 
 export  default function JsPage () {
+
     return(
         <div className={styles.container}>
-            <div className={styles.wrapper}>
-                <div className={styles.wrapperOne_Item}>
+            <div className={styles.wrapper}> 
 
-                    <div className={styles.first}>
-                        <h2 className={styles.data_h2}>Типы данных</h2>
-                        <p className={styles.data_p}>Значение в JavaScript всегда относится к данным определённого типа. Например, это может быть строка или число.
+                <div className={styles.videoBlock}>
+                    <WarningVideo/>
+                </div>
 
-                            Есть восемь основных типов данных в JavaScript. В этой главе мы рассмотрим их в общем, а в следующих главах поговорим подробнее о каждом.
+                <div className={styles.cursItemBlock}>
 
-                            Переменная в JavaScript может содержать любые данные. В один момент там может быть строка, а в другой – число:
-                        </p>
-
+                    <div className={styles.cursItem}>
+                        <h3>Циклы while и for</h3>
+                        <p>Не пройди мимо!</p>
+                        <button className={styles.btn}>Начать</button>
                     </div>
-
-                    <div className={styles.second}>
-                        <h2 className={styles.data_h2}>Число</h2>
-                        <p  className={styles.data_p}>
-                            Числовой тип данных (number) представляет как целочисленные значения, так и числа с плавающей точкой.
-
-                            Существует множество операций для чисел, например, умножение *, деление /, сложение +, вычитание - и так далее.
-
-                            Кроме обычных чисел, существуют так называемые «специальные числовые значения», которые относятся к этому типу данных: Infinity, -Infinity и NaN.
-
-                            Infinity представляет собой математическую бесконечность ∞. Это особое значение, которое больше любого числа.
-                        </p>
+                    <div className={styles.cursItem}>
+                        <h3>Функции</h3>
+                        <p>Просто о сложном</p>
+                        <button className={styles.btn}>Начать</button>
                     </div>
-                    <div className={styles.three}>
-                        <h2 className={styles.data_h2}>Число</h2>
-                        <p  className={styles.data_p}>
-                            Числовой тип данных (number) представляет как целочисленные значения, так и числа с плавающей точкой.
-
-                            Существует множество операций для чисел, например, умножение *, деление /, сложение +, вычитание - и так далее.
-
-                            Кроме обычных чисел, существуют так называемые «специальные числовые значения», которые относятся к этому типу данных: Infinity, -Infinity и NaN.
-
-                            Infinity представляет собой математическую бесконечность ∞. Это особое значение, которое больше любого числа.
-                        </p>
-                    </div>
-                    <div className={styles.four}>
-                        <h2 className={styles.data_h2}>Число</h2>
-                        <p  className={styles.data_p}>
-                            Числовой тип данных (number) представляет как целочисленные значения, так и числа с плавающей точкой.
-
-                            Существует множество операций для чисел, например, умножение *, деление /, сложение +, вычитание - и так далее.
-
-                            Кроме обычных чисел, существуют так называемые «специальные числовые значения», которые относятся к этому типу данных: Infinity, -Infinity и NaN.
-
-                            Infinity представляет собой математическую бесконечность ∞. Это особое значение, которое больше любого числа.
-                        </p>
-
-                    </div>
-                    <div className={styles.five}>
-                        <h2 className={styles.data_h2}>Число</h2>
-                        <p  className={styles.data_p}>
-                            Числовой тип данных (number) представляет как целочисленные значения, так и числа с плавающей точкой.
-
-                            Существует множество операций для чисел, например, умножение *, деление /, сложение +, вычитание - и так далее.
-
-                            Кроме обычных чисел, существуют так называемые «специальные числовые значения», которые относятся к этому типу данных: Infinity, -Infinity и NaN.
-
-                            Infinity представляет собой математическую бесконечность ∞. Это особое значение, которое больше любого числа.
-                        </p>
-
+                    <div className={styles.cursItem}>
+                        <h3>Работа с DOM</h3>
+                        <p>Почти Junior</p>
+                        <button className={styles.btn}>Начать</button>
                     </div>
                 </div>
-                <div className={styles.wrapperTwo_Item}>
-                    <h1 className={styles.data_h1}>Java Script. Циклы</h1>
 
-                    <div className={styles.content}>
-                        <div className={styles.contentOne}></div>
-                        <div className={styles.contentTwo}></div>
-
-                    </div>
-                    <p className={styles.footertitle}>NazDev corporation</p>
-                </div>
             </div>
         </div>
     )
